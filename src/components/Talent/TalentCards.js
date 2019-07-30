ee7ac7b89d5def3bcebfbb21462555544fd67ae2
LaVon Mejia <lavonmejia@gmail.com>
2019-07-29 22:13:33 -0400

    talent cards for prisoners in progress

--- src/components/Talent/TalentCards.js
+++ src/components/Talent/TalentCards.js
@@ -2,24 +2,27 @@ import React from 'react'
 import { Card, Icon, Image } from 'semantic-ui-react'
 
 export default function TalentCard (props) {
-  return   <Card>
+  return <Card>
   
   <Card.Content>
-      <h2>{props.talent.username}</h2>
-    <Card.Header>{props.name}</Card.Header>
+      
+    <Card.Header>{props.talent.name}</Card.Header>
     <Card.Meta>
-      <span className='date'>{props.HEADLINE}</span>
+      <span className='date'>{props.talent.name}</span>
     </Card.Meta>
     <Card.Description>
-    {props.status}
-    {props.CERTIFICATIONS}
-    {props.EXPERIENCE}
-    {props.PROFESSIONALGOALS}
+<p> Certifications: {props.talent.certifications[0]}, {props.talent.certifications[1]}, {props.talent.certifications[2]} </p>
+   
+   
+    <p>Goals: {props.talent.goals[0]},{props.talent.goals[1]} </p>
+   
+    <p>Pay Rate: {props.talent.payRate}</p>
+    <p>Available Start Date:{props.talent.availStart}</p>
 
     </Card.Description>
   </Card.Content>
 </Card>
-     
+ 
 }
 
 
