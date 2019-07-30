95ee2b77ee22843ac6305c514b4942bd1b040be7
LaVon Mejia <lavonmejia@gmail.com>
2019-07-30 14:26:19 -0400

    dropdown menu taking shape

---
+++ src/components/NavBarVertical.js
@@ -0,0 +1,47 @@
+
+import { Dropdown, Menu, Icon, Segment } from 'semantic-ui-react'
+import { NavLink } from "react-router-dom";
+
+import React from 'react'
+
+// TODO: Update <Search> usage after its will be implemented
+
+const MenuExampleAttached = () => (
+  <div>
+    <Menu attached='top'>
+      <Dropdown item icon='wrench' simple>
+        <Dropdown.Menu>
+          <Dropdown.Item>
+            <Icon name='dropdown' />
+            <span className='text'>New</span>
+
+            <Dropdown.Menu>
+              <Dropdown.Item>Document</Dropdown.Item>
+              <Dropdown.Item>Image</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown.Item>
+          <Dropdown.Item>Open</Dropdown.Item>
+          <Dropdown.Item>Save...</Dropdown.Item>
+          <Dropdown.Item>Edit Permissions</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Header>Export</Dropdown.Header>
+          <Dropdown.Item>Share</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+
+      <Menu.Menu position='right'>
+        <div className='ui right aligned category search item'>
+          <div className='ui transparent icon input'>
+            <input className='prompt' type='text' placeholder='Search site...' />
+            <i className='search link icon' />
+          </div>
+          <div className='results' />
+        </div>
+      </Menu.Menu>
+    </Menu>
+
+    
+  </div>
+)
+
+export default MenuExampleAttached
