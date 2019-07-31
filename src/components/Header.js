6b1c0b2fe657e265e372556898312e7f2abb4f03
LaVon Mejia <lavonmejia@gmail.com>
2019-07-31 19:37:31 -0400

    logo put in the frontend

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,10 @@ import {Image} from 'semantic-ui-react';
 
 export default function Header() {
   return <header className="ui centered">
-    <h1 className="ui right">UnBound-The Job Board that Unlocks Potential</h1>
-    
+      <div>
+      <Image size='large' src='https://mystifying-goldstine-47782b.netlify.com/img/Unhori.jpg'></Image>
+    <h1 className="ui right">The Job Board that Unlocks Potential</h1>
+    </div>
 
   </header>
 }
